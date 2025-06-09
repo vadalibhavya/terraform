a462ad0b82a04f7930990087cf5720ac5e79feae
aditya <adityavittal92@gmail.com>
2025-06-09 16:41:00 +0530

    update

--- variables/route53.tf
+++ variables/route53.tf
@@ -2,7 +2,7 @@
 resource "aws_route53_record" "create-records" {
 count = length(var.instance_name)
 zone_id = var.zone_id
-name    = "${var.instance_name[count.index]}-'internal'.${var.domain_name}"
+name    = "${var.instance_name[count.index]}-internal.${var.domain_name}"
 type    = var.type
 ttl     = var.ttl
   records = [aws_instance.roboshop[count.index].private_ip]

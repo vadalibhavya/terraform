854305ca55c2a0baf74c68137ff68104d9b8410a
aditya <adityavittal92@gmail.com>
2025-06-09 15:09:36 +0530

    update

---
+++ ec2/ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all-terraform"
+  description = "Allow all inbound traffic"
+  tags = {
+    Name = "allow_all"
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
